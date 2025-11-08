115051bf243fa32ae790fe0bf5eaea1d9bf02b98
Geshlee Ruiz <139276311+gruizg@users.noreply.github.com>
2025-11-07 19:04:27 -0500

    Add OpenMP parallelization to matrix multiplication

--- OpenMP_optimized.c
+++ OpenMP_optimized.c
@@ -13,6 +13,7 @@ double matrixMultiResult [N] [N] = {0.0};
 
 void matrixMulti()
 {
+    #pragma omp parallel for collapse(2)
     for(int row = 0 ; row < N ; row++){
         for(int col = 0; col < N ; col++){
             double resultValue = 0;
